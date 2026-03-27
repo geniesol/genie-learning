f38f8d4a27dcfb626d255183deb729f7967242b5
Genie Developer <dev@genielearning.com>
2026-03-27 17:20:47 +0530

    refactor: centralize prisma client in @genie/db

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { User, Prisma } from '@prisma/client';
+import { User, Prisma } from '@genie/db';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
